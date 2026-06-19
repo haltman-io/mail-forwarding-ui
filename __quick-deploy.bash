6bbb2920f1f0657bd93fcaa3e72bb5f03ee5a9e5
extencil <extencil@segfault.net>
2026-06-19 11:45:12 -0300

    fix

--- __quick-deploy.bash
+++ __quick-deploy.bash
@@ -6,7 +6,7 @@ npm run build
 
 echo "[+] Running: sudo rm /var/www/mail-forwarding/current/*"
 sleep 2
-sudo rm /var/www/mail-forwarding/current/*
+sudo rm /var/www/mail-forwarding/current/* -rf
 
 echo "[+] Running: sudo mv ./out/* /var/www/mail-forwarding/current/."
 sleep 2
